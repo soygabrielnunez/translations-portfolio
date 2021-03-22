4552a9745968a7211fda6c1ee0c31bf9b8692682
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-22 20:48:00 +0000

    cleansing project

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen flex flex-col justify-center items-center space-y-4">
+  <h1 class="text-xl">Próximamente, los trabajos del traductor más arrecho de Venezuela</h1>
+  <h1 class="text-5xl text-indigo-600">JULIO CÉSAR COLMENARES NATERA</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Portfolio'
+})
+</script>
