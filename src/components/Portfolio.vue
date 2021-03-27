c094772e97e75eb893af4859357897776dd6dd48
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 15:46:38 +0000

    adjusting round

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -9,28 +9,28 @@
     <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">video games</h3>
   </div>
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-2 md:p-8">
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
@@ -39,28 +39,28 @@
     <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">manga</h3>
   </div>
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-2 md:p-8">
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
@@ -69,28 +69,28 @@
     <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">novels</h3>
   </div>
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-2 md:p-8">
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
