ac53764faa71023e4e001557254c385c51328f87
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 17:52:43 +0000

    adding a new category

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -49,6 +49,21 @@
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
+  <subtitle title="NSFW" japaneseTitle="+18 作品" />
+  <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+  </div>
 </div>
 </template>
 
