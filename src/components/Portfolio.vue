d02ef127409c7af7e5fad113e5fd2b87b723e94c
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 18:27:07 +0000

    adjusting css

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,29 +1,29 @@
 <template>
-<div class="portfolio-container grid p-8 gap-10">
+<div class="grid p-8 gap-10">
   <section-title text="translations portfolio" japaneseText="翻訳ポートフォリオ" />
   <section-subtitle text="video games" japaneseText="ビデオゲーム" />
-  <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
   </div>
   <section-subtitle text="manga" japaneseText="マンガ" />
-  <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
   </div>
   <section-subtitle text="novels" japaneseText="小説" />
-  <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
   </div>
   <section-subtitle text="NSFW" japaneseText="+18 作品" />
-  <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-placeholder />
     <media-placeholder />
     <media-placeholder />
@@ -48,9 +48,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.portfolio-container {
-  grid-template-rows: auto auto;
-}
 .subtitle-container {
   background-color: #0d0216;
 }
