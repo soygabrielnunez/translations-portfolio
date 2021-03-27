650c3a0bf52ca1d6145ff9e9720baac471629099
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 09:08:34 +0000

    adding some style to the placeholders

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,16 +3,16 @@
     <h1 class="text-xl">Próximamente, los trabajos del traductor más arrecho de Venezuela</h1>
     <h1 class="text-5xl text-indigo-600">JULIO CÉSAR COLMENARES NATERA</h1>
   </div>
-  <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-20 p-8">
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
-    <div class="rounded-lg h-96 shadow-xl bg-white"></div>
+  <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-20 p-8">
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div> 
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
+    <div class="rounded-lg w-96 h-96 shadow-xl bg-white"></div>
   </div>
 </template>
 
