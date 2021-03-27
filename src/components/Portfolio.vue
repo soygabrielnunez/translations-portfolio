f02ad648f54be1959aea30422235f63b81e3802f
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 14:55:06 +0000

    adjusting style

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,18 +5,42 @@
     <h1 class="text-center text-3xl sm:text-5xl text-gray-50 font-semibold capitalize mt-4">translations portfolio</h1>
   </div>
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 md:p-8">
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div> 
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
-    <div class="rounded-lg 100% h-96 bg-gray-50"></div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
+    <div class="rounded-lg 100% h-96 bg-gray-50 flex justify-center items-center">
+      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+    </div>
   </div>
 </div>
 </template>
