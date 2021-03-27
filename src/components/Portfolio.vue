99446fcd39fafad7797ea3b54b4261d57672f465
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 17:50:43 +0000

    refactor: extracting component

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,10 +4,7 @@
     <h1 class="text-center text-3xl sm:text-5xl text-purple-400 font-extrabold tracking-widest">翻訳ポートフォリオ</h1>
     <h1 class="text-center text-3xl sm:text-5xl text-gray-50 font-semibold capitalize mt-4">translations portfolio</h1>
   </div>
-  <div class="sticky top-0 subtitle-container p-2 md:p-8">
-    <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">ビデオゲーム</h3>
-    <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">video games</h3>
-  </div>
+  <subtitle title="video games" japaneseTitle="ビデオゲーム" />
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
@@ -22,10 +19,7 @@
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
-  <div class="sticky top-0 subtitle-container p-2 md:p-8">
-    <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">マンガ</h3>
-    <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">manga</h3>
-  </div>
+  <subtitle title="manga" japaneseTitle="マンガ" />
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
@@ -40,10 +34,7 @@
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
   </div>
-  <div class="sticky top-0 subtitle-container p-2 md:p-8">
-    <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">小説</h3>
-    <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">novels</h3>
-  </div>
+  <subtitle title="novels" japaneseTitle="小説" />
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-1 md:p-8">
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
@@ -63,8 +54,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Subtitle from './Subtitle.vue'
 export default defineComponent({
-  name: 'Portfolio'
+  name: 'Portfolio',
+  components: {
+    Subtitle
+  }
 })
 </script>
 
---
+++ src/components/Subtitle.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="sticky top-0 subtitle-container p-2 md:p-8">
+  <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">{{japaneseTitle}}</h3>
+  <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">{{title}}</h3>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Subtitle',
+  props: {
+    title: String,
+    japaneseTitle: String
+  }
+})
+</script>
+
+<style scoped>
+.subtitle-container {
+  background-color: #0d0216;
+}
+</style>
