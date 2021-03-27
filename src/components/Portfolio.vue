d66a50e244c8ecdd98fd65a45dd0f5381ed85b9f
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-27 17:08:45 +0000

    reducing the amount of placeholders

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -21,18 +21,6 @@
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
   </div>
   <div class="sticky top-0 subtitle-container p-8">
     <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">マンガ</h3>
@@ -51,36 +39,12 @@
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
   </div>
   <div class="sticky top-0 subtitle-container p-8">
     <h3 class="text-xl sm:text-3xl text-purple-400 font-extrabold tracking-widest">小説</h3>
     <h3 class="text-xl sm:text-3xl text-gray-50 font-semibold capitalize mt-4">novels</h3>
   </div>
   <div class="grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 p-2 md:p-8">
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
-    <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
-      <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
-    </div>
     <div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center">
       <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
     </div>
