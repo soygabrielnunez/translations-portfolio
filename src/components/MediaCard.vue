6ded302ba1e834d69194c5d5b15f43d3ee45fb95
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-28 11:26:38 +0100

    adding year and title

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,5 +1,11 @@
 <template>
-<div class="rounded-xl 100% h-96 bg-gray-50 flex justify-center items-center bg-cover" :style=mediaUrl />
+<div>
+  <div class="rounded-xl h-96 bg-gray-50 flex justify-center items-center bg-cover" :style=mediaUrl />
+  <div class="p-2">
+    <span class="text-purple-400 font-semibold">{{year}}</span>
+    <h5 class="text-gray-50 font-semibold">{{title}}</h5>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -7,7 +13,9 @@ import { defineComponent, computed } from 'vue'
 export default defineComponent({
   name: 'MediaCard',
   props: {
-    url: String
+    url: String,
+    year: Number,
+    title: String,
   },
   setup(props) {
     const mediaUrl = computed(() => `background-image: url(${props.url})`)
