56e6fc000d248159f9b1946780891f89c4ee49dd
Gabriel Nunez <gnunez@aidacanarias.com>
2021-03-28 11:26:57 +0100

    adding more to the placeholder

--- src/components/MediaPlaceholder.vue
+++ src/components/MediaPlaceholder.vue
@@ -1,6 +1,12 @@
 <template>
-<div class="rounded-xl h-96 bg-gray-50 flex justify-center items-center">
-  <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+<div>
+  <div class="rounded-xl h-96 bg-gray-50 flex justify-center items-center">
+    <span class="text-3xl text-purple-400 font-extrabold tracking-widest">近日公開</span>
+  </div>
+  <div class="p-2">
+    <span class="text-purple-400 font-semibold">202X</span>
+    <h5 class="text-gray-50 font-semibold">Future project</h5>
+  </div>
 </div>
 </template>
 
