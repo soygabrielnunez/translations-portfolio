c1520fbdf0f48961ed7928be6cc467c7a1c8e6ad
gabrielnunez <gabriel.nzn@gmail.com>
2021-03-30 20:38:38 +0100

    limiting hover effect only on larger screens

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="transition duration-500 ease-in-out transform-gpu hover:scale-110 cursor-pointer">
+<div class="transform-none lg:transform-gpu duration-500 ease-in-out hover:scale-110 cursor-pointer">
   <a :href="infoUrl" target="_blank">
   <div class="rounded-xl h-96 bg-gray-50 flex justify-center items-center bg-cover" :style=backgroundImageStyle />
   <div class="p-2">
