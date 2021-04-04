dc07a251b93cd35da57d0f25a610df58b9d5071a
gabrielnunez <gabriel.nzn@gmail.com>
2021-04-04 11:27:13 +0100

    changing URLs

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,13 +5,13 @@
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-card
       infoUrl="https://futureradio.nekonyansoft.com/"
-      imageUrl="https://lh3.googleusercontent.com/4xpLaRDYlhhG6baA3EGEJF-3auwEvA6i5di6qqLIgZhED_HlmnioDfZ2K5zyEY0_DoD9lfjARMrCAJvs00DLPW-DapUrX8k_oEV-bxfxi2YLZlOOPb6ygefSH8QTA4GdhnW1RHyvi7M1t_dhWsxpSH0ql83sef4Z1Dpq5vdzoVXu5DFsPdNiI5ssk2AGqKYMMG-U4KP6KtIDssmwfnW0DfE65gtP-8JvQDC3My-BS_ba0uGknRpavnq8dTXfMh8jWSG-44rlvBOziLLVMTGnS2Ejt8JrWJXfgpzXNKWCJt46BCVMA-uFeLJBuZolPilxVGormDxomi91fit3o4AWEyfXWNSr0CXnlQHGoH6Jh_H7J-TlDUM8B4fNAg68Qw-H8Rs91X-w9YMtdGRnONdtgoJz0ObKhM3p_Zqzw0Xn2Gku6jeni0GdtE7CDivSNHEseNJQg_FWCjtXDq-zHOBD1FurJoyFDLB9kp9bM8tlXNuMzFpkJbVFdBK5RoJ3tk5fMe8hcqjzTb4xWWiCjEOaEkghl-A4AYoGlmo99FJM9ekb0TSozTIXA_mlQ74c39ZogxepPlPhrpXJQwMZdLDTd7m1dcB9TA2U4Hz4Hz7s-thvhDbg4_uz8KGM7XYWjggtLnC-hAjDIbwunZAqsgB49kSS5eOSKcTny2iakVHAaJ7k4BgH8SsTrlbJkpgNL6r43zzQzIQVUnG9wdYS6U9SmYhP=w632-h843-no?authuser=0"
+      imageUrl="https://lh3.googleusercontent.com/PYDPr61P2Z5lGHgX95ZSnVN5AW27MrjdM5_NunleG8435iy_Pfa540JSiE_DJ6uriQOw3WQlzt6wQtUWfQwLAzki6Yny3xiWzgOorF6StJfKK8TpaDrXjlBnXVEQegfQQfew0aKAKm1Q-ZOxRV-XXkaK0q5_Aul-m0BWdqSUlqn9aAsVttHVwzCPW6b-IN1Eg2cr-daJKA-zuq8EJzSnBYwMY2C_pbsnkMpAQtjdoWoqTGFxVUXcjHlTAguA62jB0_nz6Rakx1rCzbjL_iu9-FVDvLjBx7aD3U7nB1d6bCSvLbIyKScfQIbqxDFAPCorAORhRtxix9ai4VS1d4BaIY9Z-h_EtLXQXS4wG_DbkmsJgAuSmBecOGee_zx7jdjrFdLMejjZjHn19bBpa2T5LYrnqMWwVInWus3wXzwyL1QoY3MMSjtjvGsQ_1CHBjIgP0QX-GRbeCi8UWnTa8dilD48hr92ovofDEIeR3Y7LEY27RQxgUfzib6Rhw11tW1PHGZWKXDBS3SO37_FmPu6AIv_HU-UG3TPiThBix4f_1OiMytXO-hgOhIKsgk6UY7t6IjaAPrZVixDWJBeinMepshWR7gSpPklBRx7VtRplwa85nJT_80S3TLhm_opeBF5KtVMcu3lrueePFkpAz_CvQd_S_qhwpOl5X71RFanwgYcvLLDZQ2xfomYi9nnYM3frDjnn8MEm2R_G0d7PQlntb1s=w678-h905-no?authuser=0"
       :year="2021"
       title="The Future Radio and the Artificial Pigeons"
     />
     <media-card
       infoUrl="https://jastusa.com/eiyu-senki-gold-a-new-conquest/"
-      imageUrl="https://lh3.googleusercontent.com/ndoGnamYokEKP1EhZzmNhWO6qWXKCU9aTuN43oIgu87ZNIAbPabUOQXVal6Pm6pdE7vmuaDyHu06FUEjzXLKXeTlgJqoM_V7bqX9QrD7dVDsyx0x7GOPxxJcSM3h8xU6BeyjE34ZiIwkqs2EwNcsxWcsczt8uf8BkI2Ohsuy2iVhMfn0YomgdSJOg6bsxqXRh_eugdhcI64QzF1XP7SLkD663oFfEooHSsjupx1swMHH_tsaFxtOeETMaQL2RwspUZZzEdrcZCs8VJw69yhQmLQNoMbhYUvORQMp3ATZ4bV1077fBlsXpdVhD9_tVYoVObKNtbTN0-DOMH5V7Vd3Wn3WJxVTC7xVGAnqvU5ANXPmCaqWvAfApRiFaulxT2TKLFIB0-S7rP2tYxkJnVHoZR36N-hS4j4gQ8iumVZZiEaqoUU9ihesoWl3N4o9KtW_eJ1FBnCK6k2ayWlNhJ1tt_3-z-61HZS7l4V9j7j-rNXUGacHVcSq-WLj_PTGG5h2PYDnjMxotipHeB8iDtN4jH3nhk7TL4VR0RG8DbUJ6Jn43OskDGGZPRblf78yzGjGix9Jh_pIcRWHc5-TSgh6UlMYJHgZW98bjRwF-XnFHqVOMV18b5syMPByN32ptczE-88aC68XY5NEVeAY4Oqeax2_xxN0yy5adwczCpzESiJiLLcM0qrJrYjug4rZXAQENV27iNuZUvcVutLNFl3HQyGc=w400-h533-no?authuser=0"
+      imageUrl="https://jast.b-cdn.net/media/catalog/product/cache/2/package_image/400x/17f82f742ffe127f42dca9de82fb58b1/e/i/eiyu_senki_gold_-box.jpg"
       :year="2021"
       title="Eiyu*Senki Gold - A New Conquest"  
       adultsOnly
