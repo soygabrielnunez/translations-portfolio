b1a01922b130ae96b58f9b7d8f7a176d53fcbe4e
gabrielnunez <gabriel.nzn@gmail.com>
2021-04-04 09:25:03 +0100

    refactor: simplifying code whistl fixing image loading

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="transform-none lg:transform-gpu duration-500 ease-in-out hover:scale-110 cursor-pointer">
   <a :href="infoUrl" target="_blank">
-  <div class="rounded-xl h-96 bg-gray-50 flex justify-center items-center bg-cover" :style=backgroundImageStyle />
+  <img :src="imageUrl" class="h-96 bg-gray-50 rounded-xl" />
   <div class="p-2">
     <span class="text-purple-400 font-semibold">{{year}}</span>
     <h5 class="text-gray-50 font-semibold">{{title}}</h5>
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
+import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'MediaCard',
   props: {
@@ -19,10 +19,6 @@ export default defineComponent({
     infoUrl: String,
     year: Number,
     title: String,
-  },
-  setup(props) {
-    const backgroundImageStyle = computed(() => `background-image: url(${props.imageUrl})`)
-    return { backgroundImageStyle }
   }
 })
 </script>
