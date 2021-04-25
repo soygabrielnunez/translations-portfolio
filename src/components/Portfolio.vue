fcbcd4038bea3be735284fe4784ba158288362e3
gabrielnunez <gabriel.nzn@gmail.com>
2021-04-25 17:45:25 +0100

    adding items to the portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -10,6 +10,13 @@
       title="The Future Radio and the Artificial Pigeons"
     />
     <media-card
+      infoUrl="https://jastusa.com/world-s-horniest-housewife-rikky-horne/"
+      :imageUrl="rikkyHorne"
+      :year="2021"
+      title="Rikky Horne"
+      adultsOnly
+    />
+    <media-card
       infoUrl="https://www.johren.games/games/eiyusenkiwwx-en/"
       :imageUrl="eiyuSenkiWW"
       :year="2021"
@@ -27,7 +34,13 @@
       infoUrl="https://riddle.nekonyansoft.com/"
       :imageUrl="riddleJoker"
       :year="2020"
-      title="RiddleJoker"
+      title="Riddle Joker"
+    />
+    <media-card
+      infoUrl="https://jastusa.com/onii-kiss-onii-chan-where-s-my-kiss/"
+      :imageUrl="oniiKiss"
+      :year="2019"
+      title="Onii Kiss"
     />
     <media-card
       infoUrl="https://store.steampowered.com/app/721010/Newton_and_the_Apple_Tree/"
@@ -35,13 +48,6 @@
       :year="2018"
       title="Newton and the Apple Tree"  
     />
-  </div>
-  <section-subtitle text="manga" japaneseText="マンガ" />
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
-    <media-placeholder />
-    <media-placeholder />
-    <media-placeholder />
-    <media-placeholder />
   </div>
   <section-subtitle text="novels" japaneseText="小説" />
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
@@ -65,11 +71,13 @@ import MediaPlaceholder from './MediaPlaceholder.vue'
 import SectionTitle from './Title.vue'
 import SectionSubtitle from './Subtitle.vue'
 import futureRadioImage from './portfolio-images/the-future-radio-and-artificial-pigeons.jpg'
+import rikkyHorne from './portfolio-images/rikky-horne.jpeg'
 import eiyuSenkiGold from './portfolio-images/eiyu-senki-gold.jpg'
 import eiyuSenkiWW from './portfolio-images/eiyu-senki-war-wonder.jpeg'
 import riddleJoker from './portfolio-images/riddle-joker.jpeg'
 import newtonAndTheAppleTree from './portfolio-images/newton-and-the-apple-tree.jpg'
 import strongestGamer from './portfolio-images/strongest-gamer.jpeg'
+import oniiKiss from './portfolio-images/onii-kiss.jpeg'
 
 export default defineComponent({
   name: 'Portfolio',
@@ -81,11 +89,13 @@ export default defineComponent({
   },
   data: () => ({
     futureRadioImage,
+    rikkyHorne,
     eiyuSenkiWW,
     eiyuSenkiGold,
     riddleJoker,
     newtonAndTheAppleTree,
-    strongestGamer
+    strongestGamer,
+    oniiKiss
   })
 })
 </script>
