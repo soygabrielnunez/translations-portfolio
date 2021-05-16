ddfce444010e6ba7f3d1714ccb675cd8dab14132
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-05-16 11:18:09 +0100

    refactor: extracting portfolio items to json and rendering list from it

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,62 +4,23 @@
   <section-subtitle text="video games" japaneseText="ビデオゲーム" />
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-card
-      infoUrl="https://futureradio.nekonyansoft.com/"
-      :imageUrl="futureRadioImage"
-      :year="2021"
-      title="The Future Radio and the Artificial Pigeons"
-    />
-    <media-card
-      infoUrl="https://jastusa.com/world-s-horniest-housewife-rikky-horne/"
-      :imageUrl="rikkyHorne"
-      :year="2021"
-      title="Rikky Horne"
-      adultsOnly
-    />
-    <media-card
-      infoUrl="https://www.johren.games/games/eiyusenkiwwx-en/"
-      :imageUrl="eiyuSenkiWW"
-      :year="2021"
-      title="Eiyu＊Senki War Wonder"
-      adultsOnly
-    />
-    <media-card
-      infoUrl="https://jastusa.com/eiyu-senki-gold-a-new-conquest/"
-      :imageUrl="eiyuSenkiGold"
-      :year="2021"
-      title="Eiyu＊Senki Gold - A New Conquest"
-      adultsOnly
-    />
-    <media-card
-      :imageUrl="daydream"
-      :year="2021"
-      title="Daydream Cyanotype"
-    />
-    <media-card
-      infoUrl="https://riddle.nekonyansoft.com/"
-      :imageUrl="riddleJoker"
-      :year="2020"
-      title="Riddle Joker"
-    />
-    <media-card
-      infoUrl="https://jastusa.com/onii-kiss-onii-chan-where-s-my-kiss/"
-      :imageUrl="oniiKiss"
-      :year="2019"
-      title="Onii Kiss"
-    />
-    <media-card
-      :imageUrl="newtonAndTheAppleTree"
-      :year="2018"
-      title="Newton and the Apple Tree"  
+      v-for="(item, index) in videoGames"
+      :key="index"
+      :infoUrl="item.infoUrl"
+      :imageUrl="item.imageUrl"
+      :year="item.year"
+      :title="item.title"
     />
   </div>
   <section-subtitle text="novels" japaneseText="小説" />
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-20 p-1 md:p-8 justify-center">
     <media-card
-      infoUrl="https://www.amazon.com/dp/B07CGZZT1J"
-      :imageUrl="strongestGamer"
-      :year="2018"
-      title="Strongest Gamer"  
+      v-for="(item, index) in novels"
+      :key="index"
+      :infoUrl="item.infoUrl"
+      :imageUrl="item.imageUrl"
+      :year="item.year"
+      :title="item.title"
     />
     <media-placeholder />
     <media-placeholder />
@@ -74,15 +35,7 @@ import MediaCard from './MediaCard.vue'
 import MediaPlaceholder from './MediaPlaceholder.vue'
 import SectionTitle from './Title.vue'
 import SectionSubtitle from './Subtitle.vue'
-import futureRadioImage from './portfolio-images/the-future-radio-and-artificial-pigeons.jpg'
-import rikkyHorne from './portfolio-images/rikky-horne.jpeg'
-import eiyuSenkiGold from './portfolio-images/eiyu-senki-gold.jpg'
-import eiyuSenkiWW from './portfolio-images/eiyu-senki-war-wonder.jpeg'
-import riddleJoker from './portfolio-images/riddle-joker.jpeg'
-import newtonAndTheAppleTree from './portfolio-images/newton-and-the-apple-tree.jpg'
-import strongestGamer from './portfolio-images/strongest-gamer.jpeg'
-import oniiKiss from './portfolio-images/onii-kiss.jpeg'
-import daydream from './portfolio-images/daydream-cyanotype.jpeg'
+import portfolio from './portfolio.json'
 
 export default defineComponent({
   name: 'Portfolio',
@@ -93,15 +46,8 @@ export default defineComponent({
     MediaPlaceholder
   },
   data: () => ({
-    futureRadioImage,
-    rikkyHorne,
-    eiyuSenkiWW,
-    eiyuSenkiGold,
-    riddleJoker,
-    newtonAndTheAppleTree,
-    strongestGamer,
-    oniiKiss,
-    daydream
+    videoGames: portfolio.videoGames,
+    novels: portfolio.novels
   })
 })
 </script>
