eecc5716c90e377cb8e89a8fc16c95aa8c39071e
gabrielnunez <gabriel.nzn@gmail.com>
2021-05-16 22:14:14 +0100

    changing dark mode default value

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="grid justify-center ">
+<div class="grid justify-center">
   <header class="p-8 dark:text-gray-50 text-3xl sm:text-5xl font-semibold capitalize flex justify-between items-center">
     <div>julio <span class="text-purple-400">colmenares</span></div>
     <a href="https://twitter.com/Bango_the_Go/" target="_blank" class="transform-none lg:transform-gpu duration-500 ease-in-out hover:scale-110 cursor-pointer flex items-center">
@@ -18,21 +18,21 @@
     </a>
   </header>
   <div class="inline-flex justify-end p-8">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
       <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
     </svg>
     <Switch
-      v-model="enabled"
-      :class="enabled ? 'bg-purple-500' : 'bg-purple-400'"
-      class="relative inline-flex items-center h-6 rounded-full w-11 transition ease-in-out duration-200 mx-1"
+      v-model="enableDarkMode"
+      :class="enableDarkMode ? 'bg-purple-500' : 'bg-purple-400'"
+      class="relative inline-flex items-center h-7 w-12 rounded-full  transition ease-in-out duration-200 mx-1"
     >
       <span class="sr-only">Enable dark mode</span>
       <span
-        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-        class="inline-block w-4 h-4 transform bg-white rounded-full transition ease-in-out duration-200"
+        :class="enableDarkMode ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block w-5 h-5 transform bg-white rounded-full transition ease-in-out duration-200"
       />
     </Switch>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
     </svg>
   </div>
@@ -70,12 +70,12 @@ export default defineComponent({
     Switch
   },
   setup() {
-    const enabled = ref(true)
-    watch(enabled, (isEnabled) => {
-      isEnabled ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+    const enableDarkMode = ref(false)
+    watch(enableDarkMode, enable => {
+      enable ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
     })
     return {
-      enabled,
+      enableDarkMode,
       vueLogo
     }
   }
