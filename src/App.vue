4e9c30eccc5d5fb8cea12324442d6af43729aff0
gabrielnunez <gabriel.nzn@gmail.com>
2021-05-16 20:08:56 +0100

    switching dark mode to manual using the toggle component

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
     </a>
   </header>
   <div class="inline-flex justify-end p-8">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
       <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
     </svg>
     <Switch
@@ -32,7 +32,7 @@
         class="inline-block w-4 h-4 transform bg-white rounded-full transition ease-in-out duration-200"
       />
     </Switch>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current text-gray-900 dark:text-gray-100" viewBox="0 0 20 20" fill="currentColor">
       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
     </svg>
   </div>
@@ -60,7 +60,7 @@
 import { defineComponent } from 'vue'
 import Portfolio from './components/Portfolio.vue'
 import vueLogo from './assets/vue-logo.png'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { Switch } from '@headlessui/vue'
 
 export default defineComponent({
@@ -71,6 +71,9 @@ export default defineComponent({
   },
   setup() {
     const enabled = ref(true)
+    watch(enabled, (isEnabled) => {
+      isEnabled ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+    })
     return {
       enabled,
       vueLogo
