ab93b13c2fc6fcf1e8dc984920aacba85b034521
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-05-23 18:54:35 +0100

    adding not found page

---
+++ src/components/pages/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="h-screen w-screen bg-red-400 flex justify-center flex-col text-center">
+  <h1 class="text-3xl sm:text-5xl text-gray-50 font-extrabold tracking-widest">おっとっと</h1>
+  <br />
+  <br />
+  <p class="text-gray-50 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <router-link to="/" class="underline">come back home?</router-link></p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Portfolio from './components/pages/Portfolio.vue'
 import About from './components/pages/About.vue'
+import NotFound from './components/pages/NotFound.vue'
 
 const routes = [
     { path: '/', component: Portfolio },
-    { path: '/about-me', component: About }
+    { path: '/about-me', component: About },
+    { path: '/:pathMatch(.*)*', component: NotFound }
 ]
 
 const router = createRouter({
