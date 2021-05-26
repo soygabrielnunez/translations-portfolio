6edaf976c3b34a583d15d9dc78468a49d2ec85e9
gabrielnunez <gabriel.nzn@gmail.com>
2021-05-26 12:13:37 +0100

    unifying text style

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
 <page-layout>
   <section-title text="About me" japaneseText="私について" />
-  <p class="text-justify md:text-center text-xl dark:text-gray-50 mb-32 md:mb-20">
+  <p class="text-justify md:text-center md:text-xl dark:text-gray-50 mb-32 md:mb-20">
     I'm taking my time to tell you a bit about me, wait for it ;).
     In the meantime, feel free to <router-link to="/" class="text-purple-400 font-semibold underline">check my work</router-link>
   </p>
