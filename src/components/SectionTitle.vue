b713c84234830b4ac06bc94853795e7f249a4bc3
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-07-24 15:14:20 +0100

    adjusting title bottom margin

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="flex flex-col items-center">
   <span class="text-3xl sm:text-5xl text-purple-400 font-extrabold tracking-widest">{{japaneseText}}</span>
-  <h1 class="text-3xl sm:text-5xl dark:text-gray-50 font-semibold capitalize mt-4">{{text}}</h1>
+  <h1 class="text-3xl sm:text-5xl dark:text-gray-50 font-semibold capitalize mt-4 mb-8">{{text}}</h1>
 </div>
 </template>
 
