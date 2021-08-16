ec318c2b4caab1c595bd6e93ec4c73e8f5803181
Gabriel Nuñez <gabriel.nzn@gmail.com>
2021-08-16 20:05:21 +0100

    adding twitter profile link

--- src/components/PageLayout.vue
+++ src/components/PageLayout.vue
@@ -45,7 +45,7 @@
           </g>
         </svg>
       </a>&nbsp;
-      <span class="hidden lg:inline">Follow me: </span><span>@Bango_the_Go</span>
+      <span class="hidden lg:inline">Follow me: </span><a href="https://twitter.com/Bango_the_Go/" target="_blank" class="font-bold cursor-pointer">@Bango_the_Go</a>
       <br />
       <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 lg:h-6 lg:w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
