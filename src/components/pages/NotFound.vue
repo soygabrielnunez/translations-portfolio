7b041ed113b7ba2ffe641eab05996fbbba663337
Gabriel <gabriel.nzn@gmail.com>
2024-01-13 14:09:59 +0000

    refactor: 💬

--- src/components/pages/NotFound.vue
+++ src/components/pages/NotFound.vue
@@ -3,6 +3,6 @@
   <h1 class="text-3xl sm:text-5xl text-gray-50 font-extrabold tracking-widest">おっとっと</h1>
   <br />
   <br />
-  <p class="text-gray-50 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <router-link to="/" class="underline">come back home?</router-link></p>
+  <p class="text-gray-50 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <router-link to="/" class="underline">go back home?</router-link></p>
 </div>
 </template>
