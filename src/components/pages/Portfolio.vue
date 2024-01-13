8605ea017bab58e020b23ee96ab4e9f05a482dba
Gabriel <gabriel.nzn@gmail.com>
2024-01-13 15:37:40 +0000

    refactor: ♻️

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -1,35 +1,33 @@
 <template>
-  <div>
-    <section-title text="translations portfolio" japaneseText="翻訳ポートフォリオ" />
-    <p class="text-center dark:text-gray-50 md:text-xl"><span class="font-semibold">Hello! / <span class="text-purple-400">こんにちは</span></span> I'm a Japanese-English translator for JAST/Nekonyan/Solpress, with more than {{ yearsOfExperience }} years of experience.
-    <br />
-    You can get to know more about me <router-link to="/about-me"><span class="text-purple-400 font-semibold underline">by clicking here</span></router-link>
-    </p>
-    <section-subtitle text="video games" japaneseText="ビデオゲーム" />
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-16 p-1 md:p-8 justify-center justify-items-center">
-      <media-card
-        v-for="(item, index) in videoGames"
-        :key="index"
-        :infoUrl="item.infoUrl"
-        :imageUrl="item.imageUrl"
-        :year="item.year"
-        :title="item.title"
-        :adultsOnly="item.adultsOnly"
-      />
-    </div>
-    <section-subtitle text="novels" japaneseText="小説" />
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-16 p-1 md:p-8 justify-center justify-items-center">
-      <media-card
-        v-for="(item, index) in novels"
-        :key="index"
-        :infoUrl="item.infoUrl"
-        :imageUrl="item.imageUrl"
-        :year="item.year"
-        :title="item.title"
-        :adultsOnly="item.adultsOnly"
-      />
-      <placeholder />
-    </div>
+  <section-title text="translations portfolio" japaneseText="翻訳ポートフォリオ" />
+  <p class="text-center dark:text-gray-50 md:text-xl"><span class="font-semibold">Hello! / <span class="text-purple-400">こんにちは</span></span> I'm a Japanese-English translator for JAST/Nekonyan/Solpress, with more than {{ yearsOfExperience }} years of experience.
+  <br />
+  You can get to know more about me <router-link to="/about-me"><span class="text-purple-400 font-semibold underline">by clicking here</span></router-link>
+  </p>
+  <section-subtitle text="video games" japaneseText="ビデオゲーム" />
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-16 p-1 md:p-8 justify-center justify-items-center">
+    <media-card
+      v-for="(item, index) in videoGames"
+      :key="index"
+      :infoUrl="item.infoUrl"
+      :imageUrl="item.imageUrl"
+      :year="item.year"
+      :title="item.title"
+      :adultsOnly="item.adultsOnly"
+    />
+  </div>
+  <section-subtitle text="novels" japaneseText="小説" />
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-16 p-1 md:p-8 justify-center justify-items-center">
+    <media-card
+      v-for="(item, index) in novels"
+      :key="index"
+      :infoUrl="item.infoUrl"
+      :imageUrl="item.imageUrl"
+      :year="item.year"
+      :title="item.title"
+      :adultsOnly="item.adultsOnly"
+    />
+    <placeholder />
   </div>
 </template>
 
