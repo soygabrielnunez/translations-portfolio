ecca60cad094df0749ef5586d6b5435aee1e51cf
Gabriel <gabriel.nzn@gmail.com>
2024-01-13 15:33:12 +0000

    feat: ♿️

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,6 +1,10 @@
 <template>
-<div class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 cursor-pointer">
-  <a v-if="infoUrl" :href="infoUrl" target="_blank">
+  <a
+    v-if="infoUrl"
+    :href="infoUrl"
+    target="_blank"
+    class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 cursor-pointer"
+  >
     <img
       alt="Work's portrait image"
       :src="imageUrl"
@@ -13,18 +17,19 @@
     </div>
   </a>
   <template v-else>
-    <img
-      alt="Work's portrait image"
-      :src="imageUrl"
-      class="h-auto md:h-96 w-full rounded-xl object-cover"
-    />
-    <div class="p-2">
-      <span class="text-purple-400 font-medium">{{year}}</span>
-      <span v-if="adultsOnly" class="text-purple-400 font-medium"> (+18 作品)</span>
-      <h5 class="dark:text-gray-50 font-semibold">{{title}}</h5>
+    <div class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110">
+      <img
+        alt="Work's portrait image"
+        :src="imageUrl"
+        class="h-auto md:h-96 w-full rounded-xl object-cover"
+      />
+      <div class="p-2">
+        <span class="text-purple-400 font-medium">{{year}}</span>
+        <span v-if="adultsOnly" class="text-purple-400 font-medium"> (+18 作品)</span>
+        <h5 class="dark:text-gray-50 font-semibold">{{title}}</h5>
+      </div>
     </div>
   </template>
-</div>
 </template>
 
 <script setup lang="ts">
