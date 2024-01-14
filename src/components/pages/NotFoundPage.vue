d5e3549e507f03f7b0749287147375266acbec29
Gabriel <gabriel.nzn@gmail.com>
2024-01-14 16:19:01 +0000

    feat: ♿️ improved not found page color contrast

--- src/components/pages/NotFoundPage.vue
+++ src/components/pages/NotFoundPage.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="h-screen w-screen bg-red-400 flex justify-center flex-col text-center">
-  <h1 translate="no" class="text-3xl sm:text-5xl text-gray-50 font-extrabold tracking-widest">おっとっと</h1>
+<div class="h-screen w-screen bg-gray-50 flex justify-center flex-col text-center">
+  <h1 translate="no" class="text-3xl sm:text-5xl text-red-600 font-extrabold tracking-widest">おっとっと</h1>
   <br />
   <br />
-  <p class="text-gray-50 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <router-link to="/" class="underline">go back home?</router-link></p>
+  <p class="text-red-600 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <router-link to="/" class="underline">go back home?</router-link></p>
 </div>
 </template>
