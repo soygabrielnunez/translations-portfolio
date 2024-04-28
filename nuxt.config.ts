69c0fc9c0679d789fbea7e400aa15cf1211c0bea
Gabriel <gabriel.nzn@gmail.com>
2024-04-28 08:09:32 +0100

    feat: ✨ set color mode default value to light

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   colorMode: {
-    classSuffix: ''
+    classSuffix: '',
+    preference: 'light'
   }
 })
