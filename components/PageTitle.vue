f4557df6d55062fa16d3df9b6f4765ffbc73787b
Gabriel <gabriel.nzn@gmail.com>
2024-06-29 09:38:40 +0100

    ♻️

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,8 +1,10 @@
 <template>
-<div class="flex items-center justify-center mb-8 gap-4">
-  <h1 class="text-3xl sm:text-5xl dark:text-gray-50 font-semibold capitalize">{{text}}</h1>
-  <span translate="no" class="text-3xl sm:text-5xl text-purple-600 dark:text-purple-400 font-extrabold tracking-widest">{{japaneseText}}</span>
-</div>
+  <h1 class="text-3xl sm:text-5xl dark:text-gray-50 font-semibold capitalize mb-8 text-center">
+    {{text}}
+    <span translate="no" class="text-purple-600 dark:text-purple-400 font-extrabold tracking-widest">
+      {{japaneseText}}
+    </span>
+  </h1>
 </template>
 
 <script setup lang="ts">
