efb36579aabc1dbd9765e5b52913299551cd2d70
Gabriel <gabriel.nzn@gmail.com>
2024-07-23 06:56:54 +0100

    🐛 fixed cards size

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -3,13 +3,13 @@
     v-if="infoUrl"
     :href="infoUrl"
     target="_blank"
-    class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 cursor-pointer"
+    class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 cursor-pointer w-full"
   >
     <img
       :src="image"
       alt="Work's portrait"
       loading="lazy"
-      class="aspect-[3/4] rounded-xl"
+      class="aspect-[3/4] rounded-xl w-full"
     />
     <div class="p-2 flex flex-wrap gap-2">
       <h3 class="text-color font-semibold" translate="no">{{title}}</h3>
@@ -17,12 +17,12 @@
     </div>
   </a>
   <template v-else>
-    <div class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110">
+    <div class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 w-full">
       <img
         :src="image"
         alt="Work's portrait"
         loading="lazy"
-        class="aspect-[3/4] rounded-xl"
+        class="aspect-[3/4] rounded-xl w-full"
       />
       <div class="p-2 flex flex-wrap gap-2">
         <h3 class="text-color font-semibold" translate="no">{{title}}</h3>
