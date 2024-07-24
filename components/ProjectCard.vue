299f949ec4169db27694d883396efbf76a059740
Gabriel <gabriel.nzn@gmail.com>
2024-07-24 11:58:11 +0100

    ♻️

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -16,20 +16,20 @@
       <span v-if="isForAdultsOnly" class="text-accent font-medium"> +18</span>
     </div>
   </a>
-  <template v-else>
-    <div class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 w-full">
-      <img
-        :src="image"
-        alt="Work's portrait"
-        loading="lazy"
-        class="aspect-[3/4] rounded-xl w-full"
-      />
-      <div class="p-2 flex flex-wrap gap-2">
-        <h3 class="text-color font-semibold" translate="no">{{title}}</h3>
-        <span v-if="isForAdultsOnly" class="text-accent font-medium"> +18</span>
-      </div>
+  <div
+    v-else
+    class="transform-none lg:transform-gpu lg:duration-500 lg:ease-in-out lg:hover:scale-110 w-full">
+    <img
+      :src="image"
+      alt="Work's portrait"
+      loading="lazy"
+      class="aspect-[3/4] rounded-xl w-full"
+    />
+    <div class="p-2 flex flex-wrap gap-2">
+      <h3 class="text-color font-semibold" translate="no">{{title}}</h3>
+      <span v-if="isForAdultsOnly" class="text-accent font-medium"> +18</span>
     </div>
-  </template>
+  </div>
 </template>
 
 <script setup lang="ts">
