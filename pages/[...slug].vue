05009ae1512759dd92249fb5bb33b31f52ceec97
Gabriel <gabriel.nzn@gmail.com>
2024-07-30 18:00:38 +0100

    💄

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,7 +3,7 @@
       <h1 translate="no" class="text-3xl sm:text-5xl text-red-600 font-bold tracking-widest">おっとっと</h1>
       <br />
       <br />
-      <p class="text-red-600 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <nuxt-link to="/" class="underline">go back home?</nuxt-link></p>
+      <p class="text-red-500 md:text-xl">Oops, we couldn't find what you are looking for :( what if you <nuxt-link to="/" class="font-bold">go back home?</nuxt-link></p>
     </div>
 </template>
 <script setup lang="ts">
